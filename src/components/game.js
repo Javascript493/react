dbba5d07404ebf95f3d70e3006548b132360c5c9
heyunfei <984718946@qq.com>
2024-03-28 21:48:49 +0800

    feature# 完善井字棋游戏

--- src/components/game.js
+++ src/components/game.js
@@ -13,95 +13,114 @@ const calculateWinner = (squares) => {
     [0, 4, 8],
     [2, 4, 6]
   ]
-  for(let i = 0; i < lines.length ; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
-      return squares[a]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return { status: true, line: [a, b, c] }
     }
   }
-  return null
+  return { status: false, line: [] }
 }
 // 方块
-function Square({value, onSquareClick})  {
-  return <button className="square" onClick={onSquareClick}>{value}</button>
+function Square ({ value, line, index, onSquareClick }) {
+  const className = `square ${line.includes(index) ? 'light' : ''}`
+  return <button className={className} onClick={onSquareClick}>{value}</button>
 }
-function Board({xIsNext, squares, onPlay}) {
-  const winUser = calculateWinner(squares)
-  const curValue =  xIsNext ? 'X' : '√'
+function Board ({ xIsNext, squares, onPlay, boardSize, curMove }) {
+  const { status: winUser, line } = calculateWinner(squares)
+  const curValue = xIsNext ? 'X' : '√'
+  const [row, cell] = boardSize
   let tip = ''
-  if(winUser) {
-    tip =  `所持有${winUser}获胜`
+  if (winUser) {
+    tip = `所持有${winUser}获胜`
+  } else if (curMove === row * cell) {
+    tip = '这一局是平局没有人获胜'
   } else {
     tip = '下一步到：' + curValue
+
   }
-  function handleClick(index) {
-    if(squares[index] || winUser) return
+  function handleClick (index) {
+    if (squares[index] || winUser) return
     const nextSquares = squares.slice()
     nextSquares[index] = curValue
     onPlay(nextSquares)
   }
+
+  const getBoardRow = () => {
+    const doms = []
+    for (let i = 0; i < row; i++) {
+      const cells = []
+      for (let j = 0; j < cell; j++) {
+        const index = i * cell + j
+        cells.push((
+          <Square key={j} value={squares[index]} line={line} index={index} onSquareClick={() => { handleClick(index) }} />
+        ))
+      }
+      doms.push((
+        <div className='board-row' key={i}>
+          {cells}
+        </div>
+      ))
+    }
+    return doms
+  }
+  const boardRow = getBoardRow()
   return (
     <>
       <h1>{tip}</h1>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => {handleClick(0)}}/>
-        <Square value={squares[1]} onSquareClick={() => {handleClick(1)}}/>
-        <Square value={squares[2]} onSquareClick={() => {handleClick(2)}}/>
-      </div>
-      <div className="board-row">
-      <Square value={squares[3]}  onSquareClick={() => {handleClick(3)}}/>
-      <Square value={squares[4]} onSquareClick={() => {handleClick(4)}}/>
-      <Square value={squares[5]} onSquareClick={() => {handleClick(5)}}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => {handleClick(6)}}/>
-        <Square value={squares[7]} onSquareClick={() => {handleClick(7)}}/>
-        <Square value={squares[8]} onSquareClick={() => {handleClick(8)}}/>
-      </div>
+      {boardRow}
     </>
   );
 }
 
 export default function Game () {
-  const [curMove, setCurmove] = useState(0);
+  const [curMove, setCurmove] = useState(0)
   const xIsNext = curMove % 2 === 0
+  const [asc, setAsc] = useState(true)
   const [historys, setHistory] = useState([Array(9).fill(null)])
+  const [boardSize, setBoardSize] = useState([3, 3])
   let curSquers = historys[curMove]
-  function handlPlay(nextSquares) {
+  function handlPlay (nextSquares) {
     // 怎么玩
-    
     const nextHistorys = [...historys.slice(0, curMove + 1), nextSquares]
     setHistory(nextHistorys)
     setCurmove(nextHistorys.length - 1)
+    setHistory(nextHistorys)
   }
 
   function jumTo (move) {
     // 回退到哪一步
     setCurmove(move)
   }
-  const moves = historys.map((squares, move) => {
+  let moves = historys.map((squares, index) => {
     let desc = ''
-    if(move > 0) {
+    const move = asc ? index : historys.length - index - 1
+    if (asc ? move > 0 : move !== 0) {
       desc = '回退到这一步：' + move
     } else {
       desc = '回道开始'
     }
     return (
-      <li> 
+      <li key={move}>
         <button onClick={() => jumTo(move)}>{desc}</button>
       </li>
     )
   })
+
+  const handleReverseHistorys = () => {
+    setAsc(!asc)
+  }
   return (
-  <div className='game'>
-    <div className='content'>
-      <Board xIsNext={xIsNext} squares={curSquers} onPlay={handlPlay}></Board>
+    <div className='game'>
+      <div className='content'>
+        <Board xIsNext={xIsNext} squares={curSquers} onPlay={handlPlay} boardSize={boardSize} curMove={curMove}></Board>
+      </div>
+      <div className='history'>
+        <button onClick={handleReverseHistorys}>切换顺序</button>
+        <ul>
+          {moves}
+        </ul>
+      </div>
     </div>
-    <div className='history'>
-      <ul>
-        {moves}
-      </ul>
-    </div>
-  </div>
   )
 }
